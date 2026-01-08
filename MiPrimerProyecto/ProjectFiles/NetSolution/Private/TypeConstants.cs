48a96a5cc779c57da3c458d50b31e7fa816f25d2
Kevin Tequila Sunset <161764809+TequilaSunsets@users.noreply.github.com>
2026-01-08 17:24:39 -0600

    Ejercicios Cursos

---
+++ MiPrimerProyecto/ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,39 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace MiPrimerProyecto
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("MiPrimerProyecto");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("01a7e8933afd7c95fe894b0859308138"));
+        public static readonly NodeId Page = new NodeId(namespaceIndex, new Guid("603092339ee3de7af9818b4f90f91e3a"));
+        public static readonly NodeId AdvancedTrendConfiguration = new NodeId(namespaceIndex, new Guid("fdaa24e0443843b4b1c02f60a174773d"));
+        public static readonly NodeId AdvancedTrendPens = new NodeId(namespaceIndex, new Guid("fb0ad292c72860140f3c5833e3c0fa3a"));
+        public static readonly NodeId Dashboard = new NodeId(namespaceIndex, new Guid("524a590206384910d7328e62b2efa1b7"));
+        public static readonly NodeId VisPDF = new NodeId(namespaceIndex, new Guid("26df19542360313d3e0a5bb839342bd4"));
+        public static readonly NodeId AdvancedTrendThresholds = new NodeId(namespaceIndex, new Guid("f0f12af62d3f44e042e68c83a8005b15"));
+        public static readonly NodeId HT_Trend = new NodeId(namespaceIndex, new Guid("fee10e0fb4beb7679e9ca605b82d983d"));
+        public static readonly NodeId Data = new NodeId(namespaceIndex, new Guid("09a1359acdd2a848632908f997648c6e"));
+        public static readonly NodeId RT_Trend = new NodeId(namespaceIndex, new Guid("8d1690e50ad6265d96ff9597262781dc"));
+        public static readonly NodeId Page3 = new NodeId(namespaceIndex, new Guid("2d59d457e3c3f3f3a7ba95ce5c40a836"));
+        public static readonly NodeId AdvancedTrendMain = new NodeId(namespaceIndex, new Guid("28f16afff82338810547dd61fc95769b"));
+        public static readonly NodeId TrendTimeRange = new NodeId(namespaceIndex, new Guid("5708f69376d33df4e0d30b5b426240ea"));
+        public static readonly NodeId RangeStatistics = new NodeId(namespaceIndex, new Guid("4c907028ffc81a6db8b76be2097ace8f"));
+        public static readonly NodeId AdvancedTrendColorPicker = new NodeId(namespaceIndex, new Guid("8a54bc68188848aff84464ab0751ae0f"));
+        public static readonly NodeId AdvancedTrendThresholdUI = new NodeId(namespaceIndex, new Guid("a952f637f5ff4b9f1073c1d19967d260"));
+        public static readonly NodeId AdvancedTrendPenUI = new NodeId(namespaceIndex, new Guid("c54f3bf5c3c655257e76b1f31e3539dc"));
+        public static readonly NodeId AdvancedTrendRangeUI = new NodeId(namespaceIndex, new Guid("d671d666a8ca2ad90e7ebf5c51473a44"));
+        public static readonly NodeId AdvancedTrendRanges = new NodeId(namespaceIndex, new Guid("63dce43893097456174cce27e28ffbba"));
+        public static readonly NodeId AdvancedTrendYAxis = new NodeId(namespaceIndex, new Guid("58a444eaa2857cf709bd07f229d3313d"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("MiPrimerProyecto");
+    }
+}
---
+++ MiPrimerProyecto/ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,96 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "603092339ee3de7af9818b4f90f91e3a")]
+public class Page : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "fdaa24e0443843b4b1c02f60a174773d")]
+public class AdvancedTrendConfiguration : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "fb0ad292c72860140f3c5833e3c0fa3a")]
+public class AdvancedTrendPens : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "524a590206384910d7328e62b2efa1b7")]
+public class Dashboard : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "26df19542360313d3e0a5bb839342bd4")]
+public class VisPDF : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "f0f12af62d3f44e042e68c83a8005b15")]
+public class AdvancedTrendThresholds : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "fee10e0fb4beb7679e9ca605b82d983d")]
+public class HT_Trend : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "09a1359acdd2a848632908f997648c6e")]
+public class Data : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "8d1690e50ad6265d96ff9597262781dc")]
+public class RT_Trend : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "2d59d457e3c3f3f3a7ba95ce5c40a836")]
+public class Page3 : Page
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "28f16afff82338810547dd61fc95769b")]
+public class AdvancedTrendMain : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "8a54bc68188848aff84464ab0751ae0f")]
+public class AdvancedTrendColorPicker : FTOptix.UI.Popup
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "a952f637f5ff4b9f1073c1d19967d260")]
+public class AdvancedTrendThresholdUI : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "c54f3bf5c3c655257e76b1f31e3539dc")]
+public class AdvancedTrendPenUI : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "d671d666a8ca2ad90e7ebf5c51473a44")]
+public class AdvancedTrendRangeUI : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "63dce43893097456174cce27e28ffbba")]
+public class AdvancedTrendRanges : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "MiPrimerProyecto", Guid = "58a444eaa2857cf709bd07f229d3313d")]
+public class AdvancedTrendYAxis : FTOptix.UI.Panel
+{
+}
